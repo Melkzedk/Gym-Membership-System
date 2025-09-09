bad66b36ce3669b80d15e30710669ba18b798b62
Melkzedek <79189172+Melkzedk@users.noreply.github.com>
2025-09-09 21:26:38 +0300

    more changes

--- backend/seed.js
+++ backend/seed.js
@@ -4,7 +4,7 @@ const MembershipPlan = require("./models/MembershipPlan"); // ✅ Correct model
 
 dotenv.config();
 
-// Connect to MongoDB
+// Connect to Mongo
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
