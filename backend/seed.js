e50c37235f23cc294af1719a729df64edc1b98ef
Melkzedek <79189172+Melkzedk@users.noreply.github.com>
2025-09-10 08:15:42 +0300

    more changes

--- backend/seed.js
+++ backend/seed.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const MembershipPlan = require("./models/MembershipPlan"); 
+const MembershipPlan = require("./models/MembershipPlan");  //model
 
 dotenv.config();
 
